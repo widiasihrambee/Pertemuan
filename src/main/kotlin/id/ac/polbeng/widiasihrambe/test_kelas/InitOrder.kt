1c56d424e2454515601dfff20a1ef1838a71fe7a
widiasihrambee <widiasihrambe@gmail.com>
2022-04-02 00:03:49 +0700

    Percobaan InitOrder

---
+++ src/main/kotlin/id/ac/polbeng/widiasihrambe/test_kelas/InitOrder.kt
@@ -0,0 +1,19 @@
+package id.ac.polbeng.widiasihrambe.test_kelas
+
+
+
+class InitOrder  (name : String){
+    val firstProperty = "First property: $name".also(::println)
+    init {
+        println("First initializer block that prints $ {name}")
+   }
+    val secondProperty ="Second property: ${name.length}".also(::println)
+    init {
+        println("Second initializer block that prints ${name.length}")
+    }
+}
+fun main(){
+    val initOrder = InitOrder("RPL")
+}
+
+
