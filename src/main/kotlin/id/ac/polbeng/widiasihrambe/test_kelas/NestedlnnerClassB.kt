4ec8ec980693fbb8ad5e1661843829face5f521d
widiasihrambee <widiasihrambe@gmail.com>
2022-04-07 23:06:03 +0700

    Percobaan Anonymous Class

---
+++ src/main/kotlin/id/ac/polbeng/widiasihrambe/test_kelas/NestedlnnerClassB.kt
@@ -0,0 +1,15 @@
+package id.ac.polbeng.widiasihrambe.test_kelas
+
+fun main() {
+    // creating an instance of the interface
+    var programmer: Human = object : Human {
+        override fun think() { // overriding the think method
+            print("I am an example of Anonymous Inner Class ")
+
+        }
+    }
+    programmer.think()
+}
+interface Human {
+    fun think()
+}
