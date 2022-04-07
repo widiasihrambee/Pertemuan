942cdb4253b70dd7f1a5db291ca57b7cfb61950f
widiasihrambee <widiasihrambe@gmail.com>
2022-04-07 22:58:17 +0700

    Percobaan Interface Inheritance

---
+++ src/main/kotlin/id/ac/polbeng/widiasihrambe/test_kelas/Interface_B.kt
@@ -0,0 +1,23 @@
+package id.ac.polbeng.widiasihrambe.test_kelas
+
+interface Named {
+    val name: String
+}
+interface Person : Named {
+    val firstName: String
+    val lastName: String
+    override val name: String get() = "$firstName $lastName"
+}
+data class EmployeeB(
+
+// implementing 'name' is not required
+
+    override val firstName: String,
+    override val lastName: String,
+    val position: String
+) : Person
+
+fun main(){
+    val pegawai = EmployeeB("Widia", "Riska", "Programmer")
+    println(pegawai.name)
+}
