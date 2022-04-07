aee23b8c5875763a8d2e5930f4e44174d4fa529b
widiasihrambee <widiasihrambe@gmail.com>
2022-04-07 23:33:04 +0700

    Percobaan destructing declarations menggunakan componentN() method

---
+++ src/main/kotlin/id/ac/polbeng/widiasihrambe/test_kelas/DataClassE.kt
@@ -0,0 +1,16 @@
+package id.ac.polbeng.widiasihrambe.test_kelas
+
+import jogamp.graph.font.typecast.ot.table.Table.name
+
+fun main() {
+    fun main() {
+        val widia = Student("Widia", 21)
+
+        // Destructuring Declaration with componentN() method
+
+        val name = widia.component1()
+        val age = widia.component2()
+        println("Name = $name")
+        println("Age = $age")
+    }
+}
